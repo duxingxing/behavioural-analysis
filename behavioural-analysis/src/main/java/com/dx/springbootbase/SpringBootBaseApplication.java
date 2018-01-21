ab3937a8b6ee50b7000bd9a2d541136695a564d2
duxingxing <820967275@qq.com>
2018-01-21 20:25:00 +0800

    submmit

---
+++ behavioural-analysis/src/main/java/com/dx/springbootbase/SpringBootBaseApplication.java
@@ -0,0 +1,16 @@
+package com.dx.springbootbase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class SpringBootBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBaseApplication.class, args);
+	}
+
+}
